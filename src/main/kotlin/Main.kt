ca62af61a076d692ea956af1444dbaa45c000b30
GuilhermeCosta <guilhermercosta2004@gmail.com>
2023-03-31 10:16:23 +0100

    Added queque

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 import isel.leic.UsbPort
 import kotlin.math.*
 fun main(){
-    HAL.setBits(0b10100000)
-    val a = readln()
-    HAL.setBits(0b10100000)
-    val b = readln()
-    HAL.setBits(0b1111)
+    var queque = false
+    while (true){
+        val a = KBD.waitKey(100)
+        if (a != KBD.NONE){
+            println(a)
+            queque = true
+        }
+        if (queque){
+            HAL.setBits(128)
+        }
+        else HAL.clrBits(128)
+    }
 }
+
